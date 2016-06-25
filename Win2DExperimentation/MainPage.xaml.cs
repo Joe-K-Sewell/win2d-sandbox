2647418ba86bbf05c7a7cd1a7bf4241c0382e2cc
Joe Sewell <joe.k.sewell@gmail.com>
2016-06-25 13:21:36 -0400

    Events, MessageDialog

--- Win2DExperimentation/MainPage.xaml.cs
+++ Win2DExperimentation/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI;
 using Microsoft.Graphics.Canvas.UI.Xaml;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -31,8 +32,29 @@ namespace Win2DExperimentation
 
         void CanvasControl_Draw(CanvasControl sender, CanvasDrawEventArgs args)
         {
+            sender.DoubleTapped += Sender_DoubleTapped;
             args.DrawingSession.DrawEllipse(155, 115, 80, 30, Colors.Black, 3);
             args.DrawingSession.DrawText("Hello, world!", 100, 100, Colors.Yellow);
+        }
+
+        private async void Sender_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            var dialog = new MessageDialog("You double tapped!");
+            dialog.Title = "MessageDialog test";
+            dialog.Commands.Add(new UICommand { Label = "Sure did!", Id = 0 });
+            dialog.Commands.Add(new UICommand { Label = "Maybe...", Id = 1 });
+            dialog.DefaultCommandIndex = 0;
+            dialog.CancelCommandIndex = 1;
+            var res = await dialog.ShowAsync();
+
+            switch ((int)res.Id)
+            {
+                case 0:
+                    await new MessageDialog("You seem confident.").ShowAsync();
+                    break;
+                case 1:
+                    break;
+            }
         }
     }
 }
