18c82d70e84844c68e54d99903210d31fc497512
Joe Sewell <Joe-K-Sewell@users.noreply.github.com>
2016-07-03 17:02:25 -0400

    Implement first part of tutorial

---
+++ Tutorial/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI;
+using System.Numerics;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Tutorial
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void myCanvas_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            args.DrawingSession.DrawText("Hello, world!", 100, 100, Colors.Black);
+            args.DrawingSession.DrawCircle(125, 125, 100, Colors.Green);
+            args.DrawingSession.DrawLine(0, 0, 50, 200, Colors.Red);
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            this.myCanvas.RemoveFromVisualTree();
+            this.myCanvas = null;
+        }
+    }
+}
